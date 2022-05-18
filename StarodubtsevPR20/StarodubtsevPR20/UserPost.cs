f79e28d52cc3a4366342a2f29c14286e01b33e71
Shluha <Shluha@DESKTOP-MPSBJ0U>
2022-05-18 20:09:11 +0300

    Добавьте файлы проекта.

---
+++ StarodubtsevPR20/StarodubtsevPR20/UserPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarodubtsevPR20
+{
+    public class UserPost
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int ID { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
